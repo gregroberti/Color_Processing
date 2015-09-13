298e2bc23f4ff4acddb18d90c3a776251e49a4d6
Greg Roberti <gregroberti@gmail.com>
2015-09-13 18:21:26 -0400

    Fixing color bleeding in live preview

--- RGB_X_Arduino/RGB_X_Arduino.ino
+++ RGB_X_Arduino/RGB_X_Arduino.ino
@@ -1,3 +1,13 @@
+volatile boolean update = false;
+volatile int long TTtime;
+volatile int next_r = 0;
+volatile int next_g = 0;
+volatile int next_b = 0;
+volatile int curr_r = 0;
+volatile int curr_g = 0;
+volatile int curr_b = 0;
+
+
 void setup()
 {
   // declare the serial comm at 9600 baud rate
@@ -7,10 +17,6 @@ void setup()
   pinMode(9, OUTPUT); // red
   pinMode(6, OUTPUT); // green
   pinMode(5, OUTPUT); // blue
-
-  // another output pin o be used as GROUND 
-  //pinMode(8, OUTPUT); // ground
-  //digitalWrite(8, LOW);
 }
 
 void loop()
@@ -19,22 +25,25 @@ void loop()
   switch(GetFromSerial())
   {
   case 'R':
-    analogWrite(9, GetFromSerial());
+    next_r = GetFromSerial();
     break;
   case 'G':
-    analogWrite(6, GetFromSerial());
+    next_g = GetFromSerial();
     break;
   case 'B':
-    analogWrite(5, GetFromSerial());
+    next_b = GetFromSerial();
+    update = true;
     break;
-//  case 'C':
-//    analogWrite(5,0);
-//    analogWrite(9,0);
-//    analogWrite(6,0);
-//    delayMicroseconds(GetFromSerial());
-//    break;
-
   }
+  
+  if(update) {
+    curr_r = next_r;
+    curr_g = next_g;
+    curr_b = next_b;
+    update = false;
+  }
+
+  osmPWMSCCxyz(curr_r,curr_g,curr_b, 1);
 }
 
 // read the serial port
@@ -44,3 +53,16 @@ int GetFromSerial()
   }
   return Serial.read();
 }
+
+// this is the light engine
+void osmPWMSCCxyz(byte red, byte green, byte blue, int time)
+{ // void osmPWM
+	while (time > 0)
+	{ // while time
+		analogWrite(5, blue); // blue
+		analogWrite(6, green); // green
+		analogWrite(9, red); // red
+		time--;
+	}// elihw time
+}// diov osmPWM
+
